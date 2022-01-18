aabe51c72a70a5a05128836a621a157839410282
Muhammed Koçyiğit <muhammetkocyigit0@gmail.com>
2022-01-18 14:21:12 +0300

    merged

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Knex } from "knex";
 import { KnexNext } from "./KnexNext";
 export { SqlString } from "./implements/SqlString";
 
+
 export function knext<T>(query: Knex.QueryBuilder<T>): KnexNext<T> {
     return new KnexNext<T>(query);
 }
